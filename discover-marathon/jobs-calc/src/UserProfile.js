76db218ba66c7beb2c732ceba1f8bb1b18e1c368
Gustavo Saboya <saboya.gustavo@gmail.com>
2021-04-02 08:06:54 -0300

    Updating User Profile using EJS include method.

---
+++ discover-marathon/jobs-calc/src/UserProfile.js
@@ -0,0 +1,10 @@
+const UserProfile = {
+    name: 'Gustavo',
+    avatar: 'https://avatars.githubusercontent.com/u/64825005?v=4',
+    "monthly-budget": 3300.00,
+    "hours-per-day": 6,
+    "days-per-week": 5,
+    "vacation-per-year": 44
+};
+
+module.exports = UserProfile;
--- discover-marathon/jobs-calc/src/routes.js
+++ discover-marathon/jobs-calc/src/routes.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const routes = express.Router();
+const user = require('./UserProfile');
 
-const views = __dirname + '/views/'
+const views = __dirname + '/views/';
 
-routes.get('/', (req, res) => res.render(`${views}index`));
+
+routes.get('/', (req, res) => res.render(`${views}index`, { user }));
+
 routes.get('/job', (req, res) => res.render(`${views}job`));
+
 routes.get('/job/edit', (req, res) => res.render(`${views}job-edit`));
-routes.get('/profile', (req, res) => res.render(`${views}profile`));
+
+routes.get('/profile', (req, res) => res.render(`${views}profile`, { user }));
 
 module.exports = routes;
